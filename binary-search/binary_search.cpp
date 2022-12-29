97a779cebaecae3d2052ccbd047c8e6919272256
Logan Barnes <logan@rotor.ai>
2022-12-29 12:47:09 -0700

    binary search 2

--- binary-search/binary_search.cpp
+++ binary-search/binary_search.cpp
@@ -1,17 +1,38 @@
 #include "binary_search.h"
 
-#include <algorithm>
 #include <stdexcept>
 
 namespace binary_search {
 
-auto find(std::vector<int> const& data, int value) -> std::size_t {
-    // Cheating ;). lower_bound does the binary search.
-    auto iter = std::lower_bound(data.begin(), data.end(), value);
+// Only requires the '<' operator to be implemented.
+auto lower_bound(std::vector<int> const& data, int value) -> std::size_t {
+    std::size_t begin = 0;
+    std::size_t end   = data.size();
 
-    if (iter != data.end() && *iter == value) {
-        return static_cast<size_t>(iter - data.begin());
+    auto get_size   = [&begin, &end] { return end - begin; };
+    auto get_middle = [&begin, &end] { return (begin + end) / 2; };
+
+    for (auto middle = get_middle(), size = get_size(); // init
+         size > 0; // check
+         middle = get_middle(), size = get_size() // update
+    ) {
+        if (data[middle] < value) {
+            begin = middle + 1;
+        } else {
+            end = middle;
+        }
     }
+
+    return get_middle();
+}
+
+auto find(std::vector<int> const& data, int value) -> std::size_t {
+    auto index = lower_bound(data, value);
+
+    if (index < data.size() && data[index] == value) {
+        return index;
+    }
+
     throw std::domain_error("Item not found");
 }
 

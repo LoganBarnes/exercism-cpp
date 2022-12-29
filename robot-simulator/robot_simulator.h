5bd50e3b672dd99f30f1337e5e059f46bd45dd57
Logan Barnes <logan@rotor.ai>
2022-12-28 23:33:21 -0700

    robot simulator 3

--- robot-simulator/robot_simulator.h
+++ robot-simulator/robot_simulator.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <stdexcept>
 #include <string>
 #include <utility>
 
@@ -17,60 +16,20 @@ enum class Bearing : uint8_t {
 
 class Robot {
 public:
-    explicit Robot(Point position = {0, 0}, Bearing bearing = Bearing::NORTH)
-        : position_(position)
-        , bearing_(bearing) {}
+    explicit Robot(
+        Point position = {0, 0}, Bearing bearing = Bearing::NORTH
+    ) noexcept;
 
-    auto get_position() const -> Point const& { return position_; }
+    [[nodiscard]] auto get_position() const noexcept -> Point const&;
+    [[nodiscard]] auto get_bearing() const noexcept -> Bearing;
 
-    auto get_bearing() const -> Bearing { return bearing_; }
+    auto advance() noexcept -> void;
+    auto turn_right() noexcept -> void;
+    auto turn_left() noexcept -> void;
 
-    auto advance() -> void {
-        switch (bearing_) {
-            case Bearing::NORTH:
-                position_.second += 1;
-                break;
-            case Bearing::EAST:
-                position_.first += 1;
-                break;
-            case Bearing::SOUTH:
-                position_.second -= 1;
-                break;
-            case Bearing::WEST:
-                position_.first -= 1;
-                break;
-        }
-    }
-
-    auto turn_right() -> void {
-        auto bearing = std::underlying_type_t<Bearing>(bearing_);
-        bearing_     = static_cast<Bearing>((bearing + 1) % 4);
-    }
-
-    auto turn_left() -> void {
-        auto bearing = std::underlying_type_t<Bearing>(bearing_);
-        bearing_     = static_cast<Bearing>((bearing + 3) % 4);
-    }
-
-    auto execute_sequence(std::string const& sequence) -> void {
-        for (auto c : sequence) {
-            switch (c) {
-                case 'A':
-                    advance();
-                    break;
-                case 'R':
-                    turn_right();
-                    break;
-                case 'L':
-                    turn_left();
-                    break;
-                default:
-                    throw std::invalid_argument(
-                        "Unexpected character: '" + std::string{c} + "'"
-                    );
-            }
-        }
-    }
+    /// \throws std::invalid_argument if the string contains a character
+    ///                               that is not 'A', 'L', or 'R'.
+    auto execute_sequence(std::string const& sequence) -> void;
 
 private:
     Point   position_;

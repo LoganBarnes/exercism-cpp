c342309aae2bc3e8cb9513e02b04feda50f12f22
Logan Barnes <logan@rotor.ai>
2022-12-29 00:22:09 -0700

    Queen attack proper

--- queen-attack/queen_attack.cpp
+++ queen-attack/queen_attack.cpp
@@ -2,4 +2,40 @@
 
 namespace queen_attack {
 
-}  // namespace queen_attack
+auto operator-(Pos const& lhs, Pos const& rhs) noexcept -> Pos {
+    return std::make_pair(lhs.first - rhs.first, lhs.second - rhs.second);
+}
+
+chess_board::chess_board(Pos white, Pos black) : white_(white), black_(black) {
+    if (!chess_board::on_board(black_)) {
+        throw std::domain_error("Black piece is not on the board");
+    }
+    if (!chess_board::on_board(white_)) {
+        throw std::domain_error("White piece is not on the board");
+    }
+    if (white_ == black_) {
+        throw std::invalid_argument("White and black pieces are co-located");
+    }
+}
+
+auto chess_board::white() const noexcept -> Pos const& {
+    return white_;
+}
+
+auto chess_board::black() const noexcept -> Pos const& {
+    return black_;
+}
+
+auto chess_board::can_attack() const noexcept -> bool {
+    auto const diff           = white_ - black_;
+    auto const col_row_attack = diff.first * diff.second == 0;
+    auto const diag_attack    = std::abs(diff.first) == std::abs(diff.second);
+    return col_row_attack || diag_attack;
+}
+
+auto chess_board::on_board(Pos const& pos) noexcept -> bool {
+    auto on_board_axis = [](auto p) { return 0 <= p && p < 8; };
+    return on_board_axis(pos.first) && on_board_axis(pos.second);
+}
+
+} // namespace queen_attack
--- queen-attack/queen_attack.h
+++ queen-attack/queen_attack.h
@@ -7,32 +7,22 @@ namespace queen_attack {
 
 using Pos = std::pair<int, int>;
 
-inline auto on_board(Pos const& pos) {
-    return 0 <= pos.first && pos.first < 8 && 0 <= pos.second && pos.second < 8;
-}
+auto operator-(Pos const& lhs, Pos const& rhs) noexcept -> Pos;
 
-inline auto operator-(Pos const& lhs, Pos const& rhs) {
-    return std::make_pair(lhs.first - rhs.first, lhs.second - rhs.second);
-}
+class chess_board {
+public:
+    chess_board(Pos white, Pos black);
 
-struct chess_board {
-    chess_board(Pos white, Pos black) : white_(white), black_(black) {
-        if (!on_board(white_) || !on_board(black_)) {
-            throw std::domain_error("");
-        }
-    }
+    [[nodiscard]] auto white() const noexcept -> Pos const&;
+    [[nodiscard]] auto black() const noexcept -> Pos const&;
 
-    auto white() const -> Pos const& { return white_; }
-    auto black() const -> Pos const& { return black_; }
+    auto can_attack() const noexcept -> bool;
 
-    auto can_attack() const {
-        auto diff = white_ - black_;
-        return diff.first * diff.second == 0
-            || std::abs(diff.first) == std::abs(diff.second);
-    }
+    static auto on_board(Pos const& pos) noexcept -> bool;
 
 private:
-    Pos white_, black_;
+    Pos white_;
+    Pos black_;
 };
 
 } // namespace queen_attack

bdc5b23f1258e14c3f325487983220d2f14d9433
Logan Barnes <logan@rotor.ai>
2022-12-29 12:15:30 -0700

    raindrops 4

--- raindrops/raindrops.cpp
+++ raindrops/raindrops.cpp
@@ -1,35 +1,3 @@
 #include "raindrops.h"
 
-#include <numeric>
-#include <vector>
-
-namespace raindrops {
-
-auto convert(int input) -> std::string {
-    auto sounds = std::vector<std::pair<int, std::string>>{
-        {3, "Pling"},
-        {5, "Plang"},
-        {7, "Plong"},
-    };
-
-    if (auto result = std::accumulate(
-            sounds.begin(),
-            sounds.end(),
-            std::string{},
-            [input](auto result, auto factor_and_sound) {
-                auto [factor, sound] = factor_and_sound;
-                if (input % factor == 0) {
-                    return result + sound;
-                } else {
-                    return result;
-                }
-            }
-        );
-        !result.empty()) {
-        return result;
-    } else {
-        return std::to_string(input);
-    }
-}
-
-} // namespace raindrops
+namespace raindrops {} // namespace raindrops
--- raindrops/raindrops.h
+++ raindrops/raindrops.h
@@ -4,6 +4,15 @@
 
 namespace raindrops {
 
-auto convert(int input) -> std::string;
+inline auto convert(int input) -> std::string {
+    auto p      = std::make_pair<int, std::string>;
+    auto result = std::string{};
+
+    for (auto [f, s] : {p(3, "Pling"), p(5, "Plang"), p(7, "Plong")}) {
+        if (input % f == 0) { result += s; }
+    }
+
+    return result.empty() ? std::to_string(input) : result;
+}
 
 } // namespace raindrops

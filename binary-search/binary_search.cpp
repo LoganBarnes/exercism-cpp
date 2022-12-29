1d6aedeac89a3c8715c286f9e6678d8cb4b6fcf6
Logan Barnes <logan@rotor.ai>
2022-12-29 13:13:25 -0700

    binary search 4

--- binary-search/binary_search.cpp
+++ binary-search/binary_search.cpp
@@ -1,17 +1,3 @@
 #include "binary_search.h"
 
-#include <stdexcept>
-
-namespace binary_search {
-
-auto find(std::vector<int> const& data, int value) -> std::size_t {
-    auto iter = binary_search::lower_bound(data.begin(), data.end(), value);
-
-    if (iter != data.end() && *iter == value) {
-        return static_cast<std::size_t>(iter - data.begin());
-    }
-
-    throw std::domain_error("Item not found");
-}
-
-} // namespace binary_search
+namespace binary_search {} // namespace binary_search
--- binary-search/binary_search.h
+++ binary-search/binary_search.h
@@ -1,36 +1,19 @@
 #pragma once
 
+#include <stdexcept>
 #include <vector>
 
 namespace binary_search {
 
-/// \brief A simple replacement for std::lower_bound.
-/// \details Returns an iterator pointing to the first element
-///          in the range [first, last) that does not satisfy
-///          element < value (i.e. greater or equal to), or
-///          last if no such element is found.
-template <typename Iter, typename T>
-auto lower_bound(Iter first, Iter last, T value) -> Iter {
-    auto get_size   = [&] { return last - first; };
-    auto get_middle = [&] { return first + get_size() / 2; };
+inline auto find(std::vector<int> const& data, int value) -> std::size_t {
+    size_t b = 0, e = data.size(); // Prioritizing readability :p
 
-    auto middle = get_middle();
-
-    for (auto size = get_size(); size > 0;
-         middle = get_middle(), size = get_size()) {
-        if (*middle < value) {
-            first = middle + 1;
-        } else {
-            last = middle;
-        }
+    for (auto s = e - b, m = b + s / 2; s > 0; s = e - b, m = b + s / 2) {
+        if (data[m] == value) { return m; }
+        if (data[m] < value) { b = m + 1; } else { e = m; }
     }
 
-    return middle;
+    throw std::domain_error("Item not found");
 }
-
-/// \brief Finds the index of the first occurrence
-///        of 'value' in the sorted list 'data'.
-/// \throws std::domain_error if 'value' is not found.
-auto find(std::vector<int> const& data, int value) -> std::size_t;
 
 } // namespace binary_search

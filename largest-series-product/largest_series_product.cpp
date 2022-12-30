720bd4f35f81b51c9501d482fe2d3837b3d415e9
Logan Barnes <logan@rotor.ai>
2022-12-30 00:33:10 -0700

    largest series product 2 min

--- largest-series-product/largest_series_product.cpp
+++ largest-series-product/largest_series_product.cpp
@@ -3,29 +3,28 @@
 #include <algorithm>
 #include <cctype>
 #include <numeric>
-#include <optional>
-#include <vector>
 
 namespace largest_series_product {
 
-using namespace std;
-
-auto mult(unsigned i, char c) {
-    if (!isdigit(c)) { throw std::domain_error("Invalid digit"); }
+auto multiply(unsigned i, char c) -> unsigned {
+    if (!std::isdigit(c)) {
+        throw std::domain_error("Invalid digit");
+    }
     return i * static_cast<unsigned>(c - '0');
 }
 
-auto largest_product(string const& d, int n) -> unsigned {
-    if (n < 0 || static_cast<int>(d.size()) < n) { throw domain_error("n"); }
-
-    // Find the largest product now that all errors cases have been handled.
-    auto p = 0U;
-
-    for (auto i = n; i <= static_cast<int>(d.size()); ++i) {
-        p = max(p, accumulate(d.data() + i - n, d.data() + i, 1U, mult));
+auto largest_product(std::string const& digits, int n) -> unsigned {
+    if (n < 0 || static_cast<int>(digits.size()) < n) {
+        throw std::domain_error("Invalid span length");
     }
 
-    return p;
+    return std::transform_reduce(
+        digits.begin(),
+        digits.end() - n + 1,
+        0U,
+        [](auto l, auto r) { return std::max(l, r); },
+        [&](char const& c) { return std::accumulate(&c, &c + n, 1U, multiply); }
+    );
 }
 
 } // namespace largest_series_product

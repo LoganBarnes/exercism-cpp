78902c5bd019339cd41dbdcd6c1562b13963b25f
Logan Barnes <logan@rotor.ai>
2022-12-30 00:43:00 -0700

    largest series product 3

--- largest-series-product/largest_series_product.h
+++ largest-series-product/largest_series_product.h
@@ -1,9 +1,23 @@
 #pragma once
-
+#include <algorithm>
+#include <cctype>
+#include <numeric>
 #include <string>
 
 namespace largest_series_product {
 
-auto largest_product(std::string const& digits, int span) -> unsigned;
+inline auto multiply(unsigned i, char c) -> unsigned {
+    if (!std::isdigit(c)) { throw std::domain_error("Invalid digit"); }
+    return i * static_cast<unsigned>(c - '0');
+}
+
+inline auto largest_product(std::string const& d, int n) -> unsigned {
+    if (n < 0 || static_cast<int>(d.size()) < n) { throw std::domain_error("n"); }
+
+    return transform_reduce(d.begin(), d.end() - n + 1, 0U,
+        [](auto l, auto r) { return std::max(l, r); },
+        [&](char const& c) { return std::accumulate(&c, &c + n, 1U, multiply); }
+    );
+}
 
 } // namespace largest_series_product

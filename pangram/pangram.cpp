06a9a92df262a3b150341008d44ede2752802faa
Logan Barnes <logan@rotor.ai>
2022-12-30 13:16:51 -0700

    pangram

--- pangram/pangram.cpp
+++ pangram/pangram.cpp
@@ -1,19 +1,3 @@
 #include "pangram.h"
 
-#include <range/v3/view.hpp>
-#include <unordered_set>
-
-namespace pangram {
-
-auto is_pangram(std::string const& pangram) -> bool {
-    using namespace ranges;
-
-    auto unique_letters  = pangram
-        | views::transform([] (char c) { return std::tolower(c); })
-        | views::filter([] (char c) { return 'a' <= c && c <= 'z'; })
-        | to<std::unordered_set>();
-
-    return unique_letters.size() == 26;
-}
-
-} // namespace pangram
+namespace pangram {} // namespace pangram
--- pangram/pangram.h
+++ pangram/pangram.h
@@ -1,9 +1,16 @@
 #pragma once
-
 #include <string>
+#include <unordered_set>
 
 namespace pangram {
 
-auto is_pangram(std::string const& pangram) -> bool;
+inline auto is_pangram(std::string s) -> bool {
+    auto set = std::unordered_set<int>{};
+
+    transform(s.begin(), s.end(), s.begin(), [](char c) { return tolower(c); });
+    copy_if(s.begin(), s.end(), inserter(set, set.begin()), [](char c) { return 'a' <= c && c <= 'z'; });
+
+    return set.size() == 26;
+}
 
 }  // namespace pangram

ad75dd4483c9526b87192a7551ab6d332344f342
Logan Barnes <logan@rotor.ai>
2022-12-30 01:10:36 -0700

    matching 1

--- matching-brackets/matching_brackets.cpp
+++ matching-brackets/matching_brackets.cpp
@@ -1,9 +1,23 @@
 #include "matching_brackets.h"
 
+#include <stack>
+
 namespace matching_brackets {
 
 auto check(std::string const& input) -> bool {
-    return input.empty();
+    auto s = std::stack<char>{};
+
+    for (char c : input) {
+        switch (c) { // Attempting to format like rust :/
+        case '{': s.push('}'); break;
+        case '[': s.push(']'); break;
+        case '(': s.push(')'); break;
+        case '}': case ']': case ')':
+            if (s.empty() || s.top() != c) { return false; } else { s.pop(); }
+            break;
+        }
+    }
+    return s.empty();
 }
 
-}  // namespace matching_brackets
+} // namespace matching_brackets

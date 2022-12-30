86d93cb70df852ffe8bbb7038733d46bed28587f
Logan Barnes <logan@rotor.ai>
2022-12-29 18:15:44 -0700

    Darts 2

--- darts/darts.cpp
+++ darts/darts.cpp
@@ -1,25 +1,3 @@
 #include "darts.h"
 
-#include <array>
-#include <cmath>
-#include <map>
-#include <numeric>
-
-namespace darts {
-
-auto score(double x, double y) -> unsigned {
-    auto const distance_to_points = std::map<double, unsigned>{
-        {-1.0, 10U}, // any negative number will do.
-        {1.0, 5U},
-        {5.0, 1U},
-        {10.0, 0U},
-    };
-    auto const point    = std::array<double, 2>{x, y};
-    auto const distance = std::sqrt(
-        std::inner_product(point.begin(), point.end(), point.begin(), 0.0)
-    );
-
-    return std::prev(distance_to_points.lower_bound(distance))->second;
-}
-
-} // namespace darts
+namespace darts {} // namespace darts
--- darts/darts.h
+++ darts/darts.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <cmath>
+#include <map>
+
 namespace darts {
 
-auto score(double x, double y) -> unsigned;
+inline auto score(double x, double y) -> int { // Attempt at a "one-liner"
+    return std::prev(std::map<double, int>{{-1, 10}, {1, 5}, {5, 1}, {10, 0}}.lower_bound(std::sqrt(x * x + y * y)))->second;
+}
 
 } // namespace darts

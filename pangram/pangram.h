bb1b1d986ad0248fb7106d376d1b9655a249c25d
Logan Barnes <logan@rotor.ai>
2022-12-30 13:30:03 -0700

    full std

--- pangram/pangram.h
+++ pangram/pangram.h
@@ -1,16 +1,17 @@
 #pragma once
+#include <algorithm>
 #include <string>
 #include <unordered_set>
 
 namespace pangram {
 
-inline auto is_pangram(std::string s) -> bool {
-    auto set = std::unordered_set<int>{};
+inline auto is_pangram(std::string s) { // the scrunched version
+  auto l = std::unordered_set<int>{};
 
-    transform(s.begin(), s.end(), s.begin(), [](char c) { return tolower(c); });
-    copy_if(s.begin(), s.end(), inserter(set, set.begin()), [](char c) { return 'a' <= c && c <= 'z'; });
+  transform(s.begin(),s.end(),s.begin(),[](char c){return tolower(c);});
+  copy_if(s.begin(),s.end(),inserter(l,l.begin()),[](char c){return isalpha(c);});
 
-    return set.size() == 26;
+  return l.size() == 26;
 }
 
 }  // namespace pangram

96a3f2797c8e517bf01369e95b7cadca063ad3a2
Logan Barnes <logan@rotor.ai>
2023-01-02 22:06:01 -0700

    diff of squares minimal

--- difference-of-squares/difference_of_squares.h
+++ difference-of-squares/difference_of_squares.h
@@ -1,9 +1,20 @@
 #pragma once
 
+#include <numeric>
+
 namespace difference_of_squares {
 
-auto square_of_sum(int x) -> int;
-auto sum_of_squares(int x) -> int;
-auto difference(int x) -> int;
+struct I {
+    int value;
+    void operator++() { ++value; }
+    int& operator*() { return value; }
+    bool operator!=(I const& other) const { return value != other.value; }
+};
+
+inline int square_of_sum(int x) { return std::pow(std::accumulate(I{1}, I{x + 1}, 0), 2); }
+
+inline int sum_of_squares(int x) { return std::inner_product(I{1}, I{x + 1}, I{1}, 0); }
+
+inline int difference(int x) { return square_of_sum(x) - sum_of_squares(x); }
 
 } // namespace difference_of_squares

72ca97eace695252b5a3efd3bb352c92edafdb72
Logan Barnes <logan@rotor.ai>
2023-01-02 21:48:24 -0700

    diff of squares 1

--- difference-of-squares/difference_of_squares.cpp
+++ difference-of-squares/difference_of_squares.cpp
@@ -1,25 +1,46 @@
 #include "difference_of_squares.h"
 
-#include <algorithm>
-#include <cmath>
 #include <numeric>
-#include <vector>
+#include <type_traits>
 
 namespace difference_of_squares {
+namespace {
+
+template <typename T>
+class counting_iterator {
+public:
+    static_assert(std::is_integral_v<T>);
+
+    explicit counting_iterator(T value) : value_(value) {}
+
+    auto operator++() -> void { ++value_; }
+
+    auto operator*() -> T& { return value_; }
+
+    bool operator!=(counting_iterator const& other) const {
+        return value_ != other.value_;
+    }
+
+private:
+    T value_;
+};
+
+} // namespace
 
 auto square_of_sum(int x) -> int {
-    auto values = std::vector<int>(static_cast<unsigned>(x));
-    std::iota(values.begin(), values.end(), 1);
-    auto sum = std::accumulate(values.begin(), values.end(), 0);
+    auto sum
+        = std::accumulate(counting_iterator(1), counting_iterator(x + 1), 0);
     return sum * sum;
 }
 
 auto sum_of_squares(int x) -> int {
-    return 0;
+    return std::inner_product(
+        counting_iterator(1), counting_iterator(x + 1), counting_iterator(1), 0
+    );
 }
 
 auto difference(int x) -> int {
-    return 0;
+    return square_of_sum(x) - sum_of_squares(x);
 }
 
 } // namespace difference_of_squares

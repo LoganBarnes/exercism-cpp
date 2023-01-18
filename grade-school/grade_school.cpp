1959ef9e0e375958aec8685038e0fa27a368a4f3
Logan Barnes <logan@rotor.ai>
2023-01-18 11:38:38 -0500

    Grade-school 1

--- grade-school/grade_school.cpp
+++ grade-school/grade_school.cpp
@@ -2,4 +2,26 @@
 
 namespace grade_school {
 
-}  // namespace grade_school
+auto school::add(std::string name, Grade grade) -> void {
+    auto& names = roster_[grade];
+    if (auto iter = std::lower_bound(names.begin(), names.end(), name);
+        iter != names.end()) {
+        names.insert(iter, name);
+    } else {
+        names.emplace_back(name);
+    }
+}
+
+auto school::grade(Grade grade) const -> NameList {
+    if (auto iter = roster_.find(grade); iter != roster_.end()) {
+        return iter->second;
+    } else {
+        return {};
+    }
+}
+
+auto school::roster() const -> GradeToNamesMap const& {
+    return roster_;
+}
+
+} // namespace grade_school
--- grade-school/grade_school.h
+++ grade-school/grade_school.h
@@ -1,8 +1,24 @@
-#if !defined(GRADE_SCHOOL_H)
-#define GRADE_SCHOOL_H
+#pragma once
+
+#include <map>
+#include <string>
+#include <vector>
 
 namespace grade_school {
 
-}  // namespace grade_school
+class school {
+public:
+    using Grade           = int;
+    using NameList        = std::vector<std::string>;
+    using GradeToNamesMap = std::map<Grade, NameList>;
 
-#endif // GRADE_SCHOOL_H
+    auto add(std::string name, Grade grade) -> void;
+
+    auto grade(Grade grade) const -> NameList;
+    auto roster() const -> GradeToNamesMap const&;
+
+private:
+    GradeToNamesMap roster_;
+};
+
+} // namespace grade_school

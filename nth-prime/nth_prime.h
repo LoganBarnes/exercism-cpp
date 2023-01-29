8e5139acf33eeac4b2feade877f99b278d600687
Logan Barnes <logan@rotor.ai>
2023-01-28 23:53:02 -0500

    final

--- nth-prime/nth_prime.h
+++ nth-prime/nth_prime.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <cmath>
 #include <limits>
 #include <stdexcept>
@@ -8,10 +9,10 @@ namespace nth_prime {
 
 template <typename T>
 auto is_prime(T x) -> bool {
-    auto const max
-        = static_cast<T>(std::floor(std::sqrt(static_cast<double>(x))));
+    auto const largest_divisor = std::floor(std::sqrt(static_cast<double>(x)));
+    auto const end             = static_cast<T>(largest_divisor) + T(1);
 
-    for (auto i = T(2); i <= max; ++i) {
+    for (auto i = T(2); i < end; ++i) {
         if (x % i == 0) {
             return false;
         }
@@ -22,12 +23,16 @@ auto is_prime(T x) -> bool {
 
 template <typename T>
 auto nth(T n) -> T {
+    static_assert(std::is_integral_v<T>);
+
     if (n < 1) {
         throw std::domain_error("n must be greater than 0");
     }
 
+    constexpr auto max_i = std::numeric_limits<T>::max();
+
     auto prime = 1;
-    for (auto i = T(2); n > T(0) && i < std::numeric_limits<T>::max(); ++i) {
+    for (auto i = T(2); n > T(0) && i <= max_i; ++i) {
         if (is_prime(i)) {
             --n;
             prime = i;

57e3b135d67159e9b395913c0359229656ac77e2
Logan Barnes <logan@rotor.ai>
2023-03-01 17:00:12 -0500

    Non submitted cleanup

--- triangle/triangle.h
+++ triangle/triangle.h
@@ -1,29 +1,36 @@
 #pragma once
 
 #include <algorithm>
+#include <array>
 #include <numeric>
-#include <vector>
 
 namespace triangle {
 
 enum class flavor { equilateral, isosceles, scalene };
 
 inline auto kind(double a, double b, double c) -> flavor {
-    auto t = std::vector<double>{a, b, c};
+    auto t = std::array{a, b, c};
+
+    if (std::any_of(t.begin(), t.end(), [](auto s) { return s <= 0.0; })) {
+        throw std::domain_error("Triangle has a zero length size");
+    }
 
     auto const max_side_length  = *std::max_element(t.begin(), t.end());
     auto const sum_of_all_sides = std::accumulate(t.begin(), t.end(), 0.0);
 
-    if (std::any_of(t.begin(), t.end(), [](auto s) { return s <= 0.0; })
-        || sum_of_all_sides - max_side_length < max_side_length) {
-        throw std::domain_error("Not a triangle.");
+    if (sum_of_all_sides - max_side_length < max_side_length) {
+        throw std::domain_error("Some sides of the triangle are too short");
     }
 
-    auto p = std::vector<std::pair<double, double>>{{b, c}, {c, a}, {a, b}};
-    auto equal = [](auto sides) { return sides.first == sides.second; };
+    auto sides = std::array{b == c, c == a, a == b};
+    auto equal = [](auto b) { return b; };
 
-    if (std::all_of(p.begin(), p.end(), equal)) { return flavor::equilateral; }
-    if (std::any_of(p.begin(), p.end(), equal)) { return flavor::isosceles; }
+    if (std::all_of(sides.begin(), sides.end(), equal)) {
+        return flavor::equilateral;
+    }
+    if (std::any_of(sides.begin(), sides.end(), equal)) {
+        return flavor::isosceles;
+    }
     return flavor::scalene;
 }
 

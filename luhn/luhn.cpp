a8ec779e2ce4cbb21a8252aaa762c777ef4c4c1b
Logan Barnes <logan@rotor.ai>
2023-07-08 14:54:55 -0700

    Pushing to remote

--- luhn/luhn.cpp
+++ luhn/luhn.cpp
@@ -1,28 +1,5 @@
 #include "luhn.h"
 
-#include <algorithm>
-#include <cctype>
-#include <numeric>
-
 namespace luhn {
-
-// Doing things the 'std' way for fun
-auto valid(std::string s) -> bool {
-    auto const is_space  = [](char c) { return std::isspace(c); };
-    auto const not_digit = [](char c) { return !std::isdigit(c); };
-
-    // Remove whitespace.
-    s.erase(std::remove_if(s.begin(), s.end(), is_space), s.end());
-
-    // Check for invalid length or non-digit characters.
-    if (s.size() < 2 || std::any_of(s.begin(), s.end(), not_digit)) { return false; }
-
-    // Convert and sum the digits.
-    auto const process = [i = 0u](char c) mutable {
-        auto const digit = static_cast<size_t>(c - '0');
-        return digit + (digit - (digit > 4) * 9u) * (i++ % 2); // Attempt at a one-liner.
-    };
-    return std::transform_reduce(s.rbegin(), s.rend(), 0, std::plus<>{}, process) % 10 == 0;
-}
 
 } // namespace luhn
--- luhn/luhn.h
+++ luhn/luhn.h
@@ -1,9 +1,27 @@
 #pragma once
 
+#include <algorithm>
+#include <cctype>
+#include <numeric>
 #include <string>
 
 namespace luhn {
 
-auto valid(std::string s) -> bool;
+inline auto valid(std::string s) { // Attempting as few lines as possible.
+    s.erase(std::remove_if(s.begin(), s.end(), isspace), s.end());
+
+    if (s.size() < 2 || !std::all_of(s.begin(), s.end(), isdigit)) { return false; }
+
+    auto const process = [i = 0u](char c) mutable {
+        auto const digit = static_cast<size_t>(c - '0');
+        return digit + (digit - (digit > 4) * 9u) * (i++ % 2);
+    };
+    // This is broken on exercism servers
+    // return std::transform_reduce(s.rbegin(), s.rend(), 0, std::plus<>{}, process) % 10 == 0;
+
+    auto digits = std::vector<unsigned>(s.size());
+    std::transform(s.rbegin(), s.rend(), digits.begin(), process);
+    return std::accumulate(digits.begin(), digits.end(), 0u) % 10 == 0;
+}
 
 } // namespace luhn

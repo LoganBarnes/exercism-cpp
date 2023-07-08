f18e1940841ea0f27472679ab58a396e604af775
Logan Barnes <logan@rotor.ai>
2023-07-08 14:10:03 -0700

    luhn remove iter

--- luhn/luhn.cpp
+++ luhn/luhn.cpp
@@ -8,15 +8,21 @@ namespace luhn {
 
 // Doing things the 'std' way for fun
 auto valid(std::string s) -> bool {
-    s.erase(std::remove_if(s.begin(), s.end(), [](char c) { return std::isspace(c); }), s.end());
+    auto const is_space  = [](char c) { return std::isspace(c); };
+    auto const not_digit = [](char c) { return !std::isdigit(c); };
 
-    if (s.size() < 2) { return false; }
-    if (std::any_of(s.begin(), s.end(), [](char c) { return !std::isdigit(c); })) { return false; }
+    // Remove whitespace.
+    s.erase(std::remove_if(s.begin(), s.end(), is_space), s.end());
 
-    return std::transform_reduce(s.rbegin(), s.rend(), 0, std::plus<>{}, [i = 0u](char c) mutable {
+    // Check for invalid length or non-digit characters.
+    if (s.size() < 2 || std::any_of(s.begin(), s.end(), not_digit)) { return false; }
+
+    // Convert and sum the digits.
+    auto const process = [i = 0u](char c) mutable {
         auto const digit = static_cast<size_t>(c - '0');
         return digit + (digit - (digit > 4) * 9u) * (i++ % 2);
-    }) % 10 == 0;
+    };
+    return std::transform_reduce(s.rbegin(), s.rend(), 0, std::plus<>{}, process) % 10 == 0;
 }
 
 } // namespace luhn

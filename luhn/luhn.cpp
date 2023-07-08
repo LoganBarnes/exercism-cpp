027c9f9d72b17a1c53eb004e7ec5945036ee826f
Logan Barnes <logan@rotor.ai>
2023-07-08 14:02:20 -0700

    luhn minimal

--- luhn/luhn.cpp
+++ luhn/luhn.cpp
@@ -6,35 +6,17 @@
 
 namespace luhn {
 
-struct counting_iter {
-    size_t count;
-    auto operator++() { ++count; }
-    auto operator*() -> size_t& { return count; }
-};
-
 // Doing things the 'std' way for fun
 auto valid(std::string s) -> bool {
-    auto const is_space  = [](char c) { return std::isspace(c); };
-    auto const not_digit = [](char c) { return !std::isdigit(c); };
+    s.erase(std::remove_if(s.begin(), s.end(), [](char c) { return std::isspace(c); }), s.end());
 
-    // Remove spaces
-    s.erase(std::remove_if(s.begin(), s.end(), is_space), s.end());
+    if (s.size() < 2) { return false; }
+    if (std::any_of(s.begin(), s.end(), [](char c) { return !std::isdigit(c); })) { return false; }
 
-    // Check for invalid length or non-digits
-    if (s.size() < 2)                               { return false; }
-    if (std::any_of(s.begin(), s.end(), not_digit)) { return false; }
-
-    auto const process_digit = [](char c, size_t i) {
+    return std::transform_reduce(s.rbegin(), s.rend(), 0, std::plus<>{}, [i = 0u](char c) mutable {
         auto const digit = static_cast<size_t>(c - '0');
-        // An attempt to remove any 'if' statements.
-        return digit + (digit - (digit > 4) * 9u) * (i % 2);
-    };
-
-    // Convert and sum all digits
-    auto const sum = std::transform_reduce(
-        s.rbegin(), s.rend(), counting_iter{0}, 0, std::plus<>{}, process_digit
-    );
-    return sum % 10 == 0;
+        return digit + (digit - (digit > 4) * 9u) * (i++ % 2);
+    }) % 10 == 0;
 }
 
 } // namespace luhn

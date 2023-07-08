e5101b3edd6f2548342ad08fecc6d60b958b52ab
Logan Barnes <barneslt63@gmail.com>
2023-07-08 15:54:32 -0700

    armstrong minimal

--- armstrong-numbers/armstrong_numbers.h
+++ armstrong-numbers/armstrong_numbers.h
@@ -1,7 +1,17 @@
 #pragma once
 
+#include <cmath>
+#include <numeric>
+#include <string>
+
 namespace armstrong_numbers {
 
-auto is_armstrong_number(int n) -> bool;
+inline auto is_armstrong_number(int n) -> bool {
+    auto const s = std::to_string(n);
+    auto const l = static_cast<int>(s.length());
+    auto const convert = [l](char c) { return static_cast<int>(std::pow(c - '0', l)); };
 
-}  // namespace armstrong_numbers
+    return std::transform_reduce(s.begin(), s.end(), 0, std::plus<>{}, convert) == n;
+}
+
+} // namespace armstrong_numbers

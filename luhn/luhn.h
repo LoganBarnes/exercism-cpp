e323b6de829e34d38c33bdbbe8c5b32b5784cdfe
Logan Barnes <barneslt63@gmail.com>
2023-07-08 18:15:41 -0400

    Fix luhn on gcc

--- luhn/luhn.h
+++ luhn/luhn.h
@@ -7,21 +7,16 @@
 
 namespace luhn {
 
-inline auto valid(std::string s) { // Attempting as few lines as possible.
+inline auto valid(std::string s) { // Attempting as few (ish) lines as possible.
     s.erase(std::remove_if(s.begin(), s.end(), isspace), s.end());
 
     if (s.size() < 2 || !std::all_of(s.begin(), s.end(), isdigit)) { return false; }
 
-    auto const process = [i = 0u](char c) mutable {
+    auto const process = [end = s.data() + s.size() - 1u](char const& c) {
         auto const digit = static_cast<size_t>(c - '0');
-        return digit + (digit - (digit > 4) * 9u) * (i++ % 2);
+        return digit + (digit - (digit > 4) * 9u) * (static_cast<unsigned>(end - &c) % 2u);
     };
-    // This is broken on exercism servers
-    // return std::transform_reduce(s.rbegin(), s.rend(), 0, std::plus<>{}, process) % 10 == 0;
-
-    auto digits = std::vector<unsigned>(s.size());
-    std::transform(s.rbegin(), s.rend(), digits.begin(), process);
-    return std::accumulate(digits.begin(), digits.end(), 0u) % 10 == 0;
+    return std::transform_reduce(s.rbegin(), s.rend(), 0, std::plus<>{}, process) % 10 == 0;
 }
 
 } // namespace luhn

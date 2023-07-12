25004f35147374a133b8522ceac4b0c05678f595
Logan Barnes <logan@rotor.ai>
2023-07-11 18:47:44 -0700

    complex numbers 1

--- complex-numbers/complex_numbers.cpp
+++ complex-numbers/complex_numbers.cpp
@@ -1,5 +1,51 @@
 #include "complex_numbers.h"
 
+#include <cmath>
+
 namespace complex_numbers {
 
-}  // namespace complex_numbers
+Complex::Complex(double real, double imag) : real_(real), imag_(imag) {}
+
+auto Complex::real() const -> double {
+    return real_;
+}
+
+auto Complex::imag() const -> double {
+    return imag_;
+}
+
+auto Complex::abs() const -> double {
+    return std::sqrt(real_ * real_ + imag_ * imag_);
+}
+
+auto Complex::conj() const -> Complex {
+    return Complex{real_, -imag_};
+}
+
+auto Complex::exp() const -> Complex {
+    auto const exp_real = std::exp(real_);
+    return Complex{exp_real * std::cos(imag_), exp_real * std::sin(imag_)};
+}
+
+auto operator+(Complex const& lhs, Complex const& rhs) -> Complex {
+    return Complex{lhs.real() + rhs.real(), lhs.imag() + rhs.imag()};
+}
+
+auto operator-(Complex const& lhs, Complex const& rhs) -> Complex {
+    return Complex{lhs.real() - rhs.real(), lhs.imag() - rhs.imag()};
+}
+
+auto operator*(Complex const& lhs, Complex const& rhs) -> Complex {
+    return Complex{
+        lhs.real() * rhs.real() - lhs.imag() * rhs.imag(),
+        lhs.real() * rhs.imag() + lhs.imag() * rhs.real()};
+}
+
+auto operator/(Complex const& lhs, Complex const& rhs) -> Complex {
+    auto const denominator = rhs.real() * rhs.real() + rhs.imag() * rhs.imag();
+    return Complex{
+        (lhs.real() * rhs.real() + lhs.imag() * rhs.imag()) / denominator,
+        (lhs.imag() * rhs.real() - lhs.real() * rhs.imag()) / denominator};
+}
+
+} // namespace complex_numbers
--- complex-numbers/complex_numbers.h
+++ complex-numbers/complex_numbers.h
@@ -1,3 +1,26 @@
 #pragma once
 
-namespace complex_numbers {} // namespace complex_numbers
+namespace complex_numbers {
+
+class Complex {
+public:
+    Complex(double real, double imag);
+
+    auto real() const -> double;
+    auto imag() const -> double;
+
+    auto abs() const -> double;
+    auto conj() const -> Complex;
+    auto exp() const -> Complex;
+
+private:
+    double real_;
+    double imag_;
+};
+
+auto operator+(Complex const& lhs, Complex const& rhs) -> Complex;
+auto operator-(Complex const& lhs, Complex const& rhs) -> Complex;
+auto operator*(Complex const& lhs, Complex const& rhs) -> Complex;
+auto operator/(Complex const& lhs, Complex const& rhs) -> Complex;
+
+} // namespace complex_numbers

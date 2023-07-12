c809874bbbe304357c582b893f266529c9fcacaa
Logan Barnes <logan@rotor.ai>
2023-07-11 21:06:02 -0700

    food chain 1

--- food-chain/food_chain.cpp
+++ food-chain/food_chain.cpp
@@ -13,46 +13,17 @@ struct Verse {
 };
 
 constexpr auto verses_list = std::array{
-    Verse{
-        "fly",
-        "I don't know why she swallowed the fly. Perhaps she'll die.",
-        "",
-    },
+    Verse{"fly", "I don't know why she swallowed the fly. Perhaps she'll die.", ""},
     Verse{
         "spider",
         "It wriggled and jiggled and tickled inside her.",
-        " that wriggled and jiggled and tickled inside her",
-    },
-    Verse{
-        "bird",
-        "How absurd to swallow a bird!",
-        "",
-    },
-    Verse{
-        "cat",
-        "Imagine that, to swallow a cat!",
-        "",
-    },
-    Verse{
-        "dog",
-        "What a hog, to swallow a dog!",
-        "",
-    },
-    Verse{
-        "goat",
-        "Just opened her throat and swallowed a goat!",
-        "",
-    },
-    Verse{
-        "cow",
-        "I don't know how she swallowed a cow!",
-        "",
-    },
-    Verse{
-        "horse",
-        "She's dead, of course!",
-        "",
-    },
+        " that wriggled and jiggled and tickled inside her"},
+    Verse{"bird", "How absurd to swallow a bird!", ""},
+    Verse{"cat", "Imagine that, to swallow a cat!", ""},
+    Verse{"dog", "What a hog, to swallow a dog!", ""},
+    Verse{"goat", "Just opened her throat and swallowed a goat!", ""},
+    Verse{"cow", "I don't know how she swallowed a cow!", ""},
+    Verse{"horse", "She's dead, of course!", ""},
 };
 
 } // namespace
@@ -62,25 +33,24 @@ auto verse(int const n) -> std::string {
         throw std::invalid_argument("'n' must be in the range [1,8]");
     }
 
-    auto const to_index = [](int i) { return static_cast<unsigned>(i) - 1ul; };
-
     auto stream = std::stringstream{};
 
-    {
-        auto v = verses_list[to_index(n)];
+    auto const index      = static_cast<unsigned>(n) - 1ul;
+    auto       verse_data = verses_list[index];
 
-        stream << "I know an old lady who swallowed a " << v.animal << ".\n"
-               << v.exclamation << "\n";
-    }
+    stream << "I know an old lady who swallowed a " << verse_data.animal << ".\n"
+           << verse_data.exclamation << "\n";
 
     if (n == 1 || n == 8) {
         return stream.str();
     }
 
-    for (auto i = n; i > 1; --i) {
-        stream << "She swallowed the " << verses_list[to_index(i)].animal
-               << " to catch the " << verses_list[to_index(i - 1)].animal
-               << verses_list[to_index(i - 1)].action << ".\n";
+    for (auto i = index; i > 0u; --i) {
+        auto const current  = verses_list[i];
+        auto const previous = verses_list[i - 1ul];
+
+        stream << "She swallowed the " << current.animal << " to catch the "
+               << previous.animal << previous.action << ".\n";
     }
 
     stream << verses_list[0].exclamation << "\n";

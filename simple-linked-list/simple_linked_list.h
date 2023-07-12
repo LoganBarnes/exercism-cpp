095c5005c7959d5c04992ed51cec1ef71ef1b38d
Logan Barnes <logan@rotor.ai>
2023-07-12 11:54:22 -0700

    Add linked list

---
+++ simple-linked-list/simple_linked_list.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <cstddef>
+
+namespace simple_linked_list {
+
+class List {
+public:
+    List() = default;
+    ~List();
+
+    // Moving and copying is not needed to solve the exercise.
+    // If you want to change these, make sure to correctly
+    // free / move / copy the allocated resources.
+    List(List const&)            = delete;
+    List& operator=(List const&) = delete;
+    List(List&&)                 = delete;
+    List& operator=(List&&)      = delete;
+
+    std::size_t size() const;
+    void        push(int entry);
+    int         pop();
+    void        reverse();
+
+private:
+    struct Element {
+        explicit Element(int data) : data{data} {};
+        int      data = 0;
+        Element* next = nullptr;
+    };
+
+    [[maybe_unused]] Element*    head_         = nullptr;
+    [[maybe_unused]] std::size_t current_size_ = 0;
+};
+
+} // namespace simple_linked_list

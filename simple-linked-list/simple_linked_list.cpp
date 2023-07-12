5bf4b0fddbe8d33ae8a5006014c658cdd1670e9b
Logan Barnes <logan@rotor.ai>
2023-07-12 12:03:40 -0700

    linked list 1

--- simple-linked-list/simple_linked_list.cpp
+++ simple-linked-list/simple_linked_list.cpp
@@ -5,28 +5,46 @@
 namespace simple_linked_list {
 
 std::size_t List::size() const {
-    // TODO: Return the correct size of the list.
-    return 0;
+    return current_size_;
 }
 
-void List::push(int /*entry*/) {
-    // TODO: Implement a function that pushes an Element with `entry` as data to
-    // the front of the list.
+void List::push(int entry) {
+    auto node  = new Element{entry};
+    node->next = head_;
+    head_      = node;
+    ++current_size_;
 }
 
 int List::pop() {
-    // TODO: Implement a function that returns the data value of the first
-    // element in the list then discard that element.
-    return 0;
+    int entry = head_->data;
+
+    auto head = head_;
+    head_     = head_->next;
+    delete head;
+    --current_size_;
+
+    return entry;
 }
 
 void List::reverse() {
-    // TODO: Implement a function to reverse the order of the elements in the
-    // list.
+    Element* prev = nullptr;
+    Element* curr = head_;
+    Element* next = nullptr;
+
+    while (curr) {
+        next       = curr->next;
+        curr->next = prev;
+        prev       = curr;
+        curr       = next;
+    }
+
+    head_ = prev;
 }
 
 List::~List() {
-    // TODO: Ensure that all resources are freed on destruction
+    while (head_) {
+        pop();
+    }
 }
 
-}  // namespace simple_linked_list
+} // namespace simple_linked_list

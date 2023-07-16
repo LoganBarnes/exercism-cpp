bfc91a51b17b7c971deef5b936e7e518e1c7f411
Logan Barnes <logan@rotor.ai>
2023-07-15 22:03:59 -0700

    binary 1

---
+++ binary/binary.cpp
@@ -0,0 +1,27 @@
+#include "binary.h"
+
+#include <algorithm>
+#include <numeric>
+
+namespace binary {
+
+auto convert(std::string_view input) -> int {
+    auto result = 0;
+
+    for (auto const& c : input) {
+        // Switches just seem cleaner to me.
+        // Maybe I've been writing too much rust.
+        switch (c) {
+            case '0':
+            case '1':
+                result = (result << 1) | (c - '0');
+                break;
+            default:
+                return 0;
+        }
+    }
+
+    return result;
+}
+
+} // namespace binary
---
+++ binary/binary.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+namespace binary {
+
+auto convert(std::string_view input) -> int;
+
+} // namespace binary

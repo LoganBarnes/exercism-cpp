7fbedffc82631727ef80d8cb8fb1e93eb18c3ada
Logan Barnes <logan@rotor.ai>
2023-07-17 01:55:35 -0600

    roman alt

--- roman-numerals/roman_numerals.cpp
+++ roman-numerals/roman_numerals.cpp
@@ -2,31 +2,30 @@
 
 #include <array>
 #include <stdexcept>
-#include <vector>
 
 namespace roman_numerals {
 namespace {
 
 constexpr auto roman_chars = std::array{'I', 'V', 'X', 'L', 'C', 'D', 'M'};
 
-auto convert_digit(unsigned digit, char ones_char, char five_or_tens_char) {
+auto convert_digit(char digit, char ones_char, char five_or_tens_char) {
     auto roman_number = std::string{};
 
     switch (digit) {
-        case 3u:
+        case '3':
             roman_number += ones_char; // fallthrough
-        case 2u:
+        case '2':
             roman_number += ones_char; // fallthrough
-        case 1u:
+        case '1':
             roman_number += ones_char;
             break;
 
-        case 4u:
+        case '4':
             roman_number += ones_char; // fallthrough
-        case 5u: // fallthrough
-        case 6u: // fallthrough
-        case 7u: // fallthrough
-        case 8u:
+        case '5': // fallthrough
+        case '6': // fallthrough
+        case '7': // fallthrough
+        case '8':
             roman_number += five_or_tens_char;
             break;
 
@@ -44,11 +43,8 @@ auto convert(unsigned i) -> std::string {
         throw std::domain_error{"i must be positive and less than 3,999"};
     }
 
-    auto digits = std::vector<unsigned>{};
-
-    for (; i > 0; i /= 10) {
-        digits.emplace_back(i % 10u);
-    }
+    auto digits = std::to_string(i);
+    std::reverse(digits.begin(), digits.end());
 
     auto roman_number = std::string{};
 
@@ -61,8 +57,8 @@ auto convert(unsigned i) -> std::string {
 
         roman_number += convert_digit(digit, *ones_iter, *fives_or_tens_iter);
 
-        if (digit > 5) {
-            digits.emplace_back(digit - 5);
+        if (digit > '5') {
+            digits += digit - 5;
             fives_or_tens_iter = ones_iter + 2u;
         } else {
             ones_iter -= 2u;

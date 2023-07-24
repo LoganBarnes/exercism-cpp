2844e604fb4c2ff8b9f9740074c9cdb302b9c626
Logan Barnes <logan@rotor.ai>
2023-07-24 01:24:50 -0700

    word count 2

--- word-count/word_count.cpp
+++ word-count/word_count.cpp
@@ -17,42 +17,25 @@ auto is_word(char ch) {
 auto words(std::string const& phrase) -> std::map<std::string, int> {
     auto result = std::map<std::string, int>{};
 
-    if (phrase.empty()) {
-        return result;
-    }
+    auto word = std::string{};
 
-    struct CharState {
-        char c       = '\0';
-        bool is_word = false;
-    };
+    for (auto ch : phrase) {
+        auto c = to_lower(ch);
 
-    CharState curr = {phrase.front(), is_word(phrase.front())};
-    CharState next;
-    auto      word = std::string{};
-
-    for (auto i = 1u; i < phrase.size(); ++i) {
-        next = {phrase[i], is_word(phrase[i])};
-
-        if (curr.is_word) {
-            word += to_lower(curr.c);
+        if (is_word(c)) {
+            word += c;
         } else if (!word.empty()) {
-            if (curr.c == '\'' && next.is_word) {
-                word += curr.c;
+            if (c == '\'') {
+                word += c;
             } else {
-                ++result[word];
+                ++result[word.substr(0, word.size() - (word.back() == '\''))];
                 word.clear();
             }
         }
-
-        curr = next;
-    }
-
-    if (curr.is_word) {
-        word += to_lower(curr.c);
     }
 
     if (!word.empty()) {
-        ++result[word];
+        ++result[word.substr(0, word.size() - (word.back() == '\''))];
     }
 
     return result;

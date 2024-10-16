0d2cd47aafad7394d2e58170db33e1a89af10ab8
Logan Barnes <logan@rotor.ai>
2024-10-15 23:40:46 -0600

    Handle missing boost and simplify binary search

--- binary-search/binary_search.h
+++ binary-search/binary_search.h
@@ -8,7 +8,7 @@ namespace binary_search {
 inline auto find(std::vector<int> const& data, int value) -> std::size_t {
     size_t b = 0, e = data.size(); // Prioritizing readability :p
 
-    for (auto s = e - b, m = b + s / 2; s > 0; s = e - b, m = b + s / 2) {
+    for (auto s = e, m = s / 2; s > 0; s = e - b, m = b + s / 2) {
         if (data[m] == value) { return m; }
         if (data[m] < value) { b = m + 1; } else { e = m; }
     }

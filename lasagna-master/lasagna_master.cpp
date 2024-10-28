e671446be412385d4fd70a4baa9958db58bab34e
Logan Barnes <logan@rotor.ai>
2024-10-27 23:29:57 -0600

    Add lasagna_master

--- lasagna-master/lasagna_master.cpp
+++ lasagna-master/lasagna_master.cpp
@@ -1,6 +1,69 @@
 #include "lasagna_master.h"
 
+#include <algorithm>
+#include <iterator>
+#include <string_view>
+
 namespace lasagna_master {
+namespace {
 
+struct IsQuestionMark {
+    auto operator()(std::string_view ingredient) const {
+        return ingredient == "?";
+    }
+};
 
-}  // namespace lasagna_master
+struct Scale {
+    double factor = 1.0;
+
+    auto operator()(double const quantity) const { return quantity * factor; }
+};
+
+} // namespace
+
+auto preparationTime(Ingredients const& layers) -> int {
+    return preparationTime(layers, 2);
+}
+
+auto preparationTime(Ingredients const& layers, int time_per_layer) -> int {
+    return static_cast<int>(layers.size()) * time_per_layer;
+}
+
+auto quantities(Ingredients const& layers) -> amount {
+    return amount{
+        // .noodles =
+        static_cast<int>(std::count(layers.begin(), layers.end(), "noodles"))
+            * 50,
+        // .sauce =
+        static_cast<double>(std::count(layers.begin(), layers.end(), "sauce"))
+            * 0.2,
+    };
+}
+
+auto addSecretIngredient(Ingredients myList, Ingredients const& friendsList)
+    -> void {
+    return addSecretIngredient(myList, friendsList.back());
+}
+
+auto addSecretIngredient(Ingredients myList, std::string const& ingredient)
+    -> void {
+    std::replace_if(myList.begin(), myList.end(), IsQuestionMark{}, ingredient);
+}
+
+auto scaleRecipe(Quantities const& quantities_for_two, double desired_portions)
+    -> Quantities {
+
+    auto quantities = Quantities{};
+    quantities.reserve(quantities_for_two.size());
+
+    std::transform(
+        quantities_for_two.begin(),
+        quantities_for_two.end(),
+        std::back_inserter(quantities),
+        Scale{desired_portions / 2.0}
+    );
+
+    return quantities;
+}
+
+} // namespace lasagna_master
--- lasagna-master/lasagna_master.h
+++ lasagna-master/lasagna_master.h
@@ -1,10 +1,41 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 namespace lasagna_master {
 
+using Ingredients = std::vector<std::string>;
+using Quantities  = std::vector<double>;
+
 struct amount {
-    int noodles;
-    double sauce;
+    // grams
+    int noodles = 0;
+    // liters
+    double sauce = 0.0;
 };
 
-}  // namespace lasagna_master
+/// \brief Calculate the preparation time (in minutes) for a
+///        lasagna which takes 2 min to prepare each layer.
+auto preparationTime(Ingredients const& layers) -> int;
+
+/// \brief Calculate the preparation time (in minutes) for a lasagna.
+auto preparationTime(Ingredients const& layers, int min_per_layer) -> int;
+
+/// \brief Calculate the amount of noodles and sauce needed for a lasagna.
+auto quantities(Ingredients const& layers) -> amount;
+
+/// \brief Add a friend's secret ingredient to the lasagna.
+auto addSecretIngredient(Ingredients& myList, Ingredients const& friendsList)
+    -> void;
+
+/// \brief Add the family secret ingredient to the lasagna.
+auto addSecretIngredient(Ingredients& myList, std::string const& ingredient)
+    -> void;
+
+/// \brief Determine the quantities of ingredients needed to
+///        produce the desired number of portions.
+auto scaleRecipe(Quantities const& quantities_for_two, double desired_portions)
+    -> Quantities;
+
+} // namespace lasagna_master

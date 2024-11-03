b9fdd992636cb4903001e0c6224b70a6142e16e2
Logan Barnes <logan@rotor.ai>
2024-11-02 23:56:10 -0600

    C++23 diamond

--- diamond/diamond.cpp
+++ diamond/diamond.cpp
@@ -6,22 +6,23 @@
 
 namespace diamond {
 
+using namespace std;
+
 auto mirror(auto value) {
-    value.insert(value.end(), value.rbegin() + 1, value.rend());
-    return value;
+    using R = decltype(value);
+    // C++26 use views::concat instead of views::join
+    return views::join(vector{value, value | views::reverse | views::drop(1) | ranges::to<R>()})
+         | ranges::to<R>();
 }
 
-auto rows(char const c) -> std::vector<std::string> {
-    auto const n = static_cast<size_t>(c - 'A');
+auto rows(char const c) -> vector<string> {
+    auto const n = static_cast<size_t>(c - 'A') + 1UL;
 
-    auto const result
-        = std::views::iota(0UL, n + 1UL)
-        | std::views::transform([n](auto const i) {
-              return mirror(std::format("{:>{}c}{}", 'A' + i, n - i + 1UL, std::string(i, ' ')));
-          })
-        | std::ranges::to<std::vector>();
+    auto const to_row = [n](auto const row) {
+        return mirror(format("{:>{}c}{}", 'A' + row, n - row, string(row, ' ')));
+    };
 
-    return mirror(result);
+    return mirror(views::iota(0UL, n) | views::transform(to_row) | ranges::to<vector>());
 }
 
 } // namespace diamond

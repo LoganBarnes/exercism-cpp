1441c81982942c76550b72ee1bf98932f01017f8
Logan Barnes <logan@rotor.ai>
2024-11-03 01:17:09 -0600

    std diamond

--- diamond/diamond.cpp
+++ diamond/diamond.cpp
@@ -1,9 +1,12 @@
 #include "diamond.h"
 
+#include <algorithm>
+#include <numeric>
 #include <stdexcept>
 
 namespace diamond {
-namespace {
+
+using namespace std;
 
 template <typename Container>
 auto mirror(Container const& value) {
@@ -12,21 +15,26 @@ auto mirror(Container const& value) {
     return copy;
 };
 
-} // namespace
-
-auto rows(char const c) -> std::vector<std::string> {
+auto rows(char const c) -> vector<string> {
     if ((c < 'A') || ('Z' < c)) {
         throw std::domain_error("Invalid input");
     }
 
     auto const n = static_cast<size_t>(c - 'A') + 1UL;
 
-    auto result = std::vector<std::string>(n, std::string(n, ' '));
+    auto const to_row = [n](auto const row) {
+        return mirror(
+            std::string(n - row - 1UL, ' ')
+            + static_cast<char>('A' + row)
+            + std::string(row, ' ')
+        );
+    };
 
-    for (auto i = 0UL; i < n; ++i) {
-        result[i][n - i - 1UL] = static_cast<char>('A' + i);
-        result[i]              = mirror(result[i]);
-    }
+    auto indices = std::vector<size_t>(n);
+    auto result  = std::vector<std::string>(n);
+
+    std::iota(indices.begin(), indices.end(), 0UL);
+    std::transform(indices.begin(), indices.end(), result.begin(), to_row);
 
     return mirror(result);
 }

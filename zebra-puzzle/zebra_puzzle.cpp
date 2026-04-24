2de664af1f685b1a15e772294c806b2606985ffc
Logan Barnes <logan@rotor.ai>
2026-04-24 13:36:07 -0600

    Don't remember [broken]

--- zebra-puzzle/zebra_puzzle.cpp
+++ zebra-puzzle/zebra_puzzle.cpp
@@ -1,91 +1,142 @@
 #include "zebra_puzzle.h"
 
+#include <algorithm>
 #include <array>
 #include <functional>
+#include <map>
+#include <ranges>
+#include <set>
 #include <variant>
 #include <vector>
 
 namespace zebra_puzzle {
 namespace {
 
+constexpr auto* nationality = "nationality";
+constexpr auto* color       = "color";
+constexpr auto* pet         = "pet";
+constexpr auto* location    = "location";
+constexpr auto* drink       = "drink";
+constexpr auto* hobby       = "hobby";
+
+using HouseElement = std::pair<char const*, std::string>;
+using ElementSet   = std::set<HouseElement>;
+
 struct House {
-    std::string nationality = {};
-    std::string color       = {};
-    int         location    = {};
-    std::string pet         = {};
-    std::string drink       = {};
-    std::string hobby       = {};
+    std::map<char const*, std::string>           elements;
+    std::map<char const*, std::set<std::string>> potential_elements;
 };
 
-struct HousePossibilities {
-    std::vector<std::string> nationality = {};
-    std::vector<std::string> color       = {};
-    std::vector<int>         location    = {};
-    std::vector<std::string> pet         = {};
-    std::vector<std::string> drink       = {};
-    std::vector<std::string> hobby       = {};
+struct Constraint {
+    HouseElement                                   house;
+    std::function<ElementSet(HouseElement const&)> constraint;
 };
 
-auto right_house(House const& house) -> HousePossibilities {
-    return {.location = {house.location + 1}};
-}
-
-auto adjacent_house(House const& house) -> HousePossibilities {
-    if (house.location == 0) {
-        return {.location = {1}};
-    } else if (house.location == 4) {
-        return {.location = {3}};
+auto right_house(HouseElement const& house) -> ElementSet {
+    if (auto const number = std::stoi(house.second); number < 5) {
+        return ElementSet{{location, std::to_string(number + 1)}};
     } else {
-        return {.location = {house.location - 1, house.location + 1}};
+        throw std::domain_error("House location out of range");
     }
 }
 
-using Functor = std::function<HousePossibilities(House)>;
+auto adjacent_house(House const& house) -> ElementSet {
+    if (house.elements.contains())
+    auto const number = std::stoi(house.second);
 
-struct Constraint {
-    House                                     input  = {};
-    std::variant<HousePossibilities, Functor> output = {};
-};
-
-struct Spec {
-    House   inputs;
-    Functor outputs;
-};
-
-using Const = std::variant<House, Spec>;
+    auto result = std::set<HouseElement>();
+    if (number > 1) {
+        result.emplace(location, std::to_string(number - 1));
+    }
+    if (number < 5) {
+        result.emplace(location, std::to_string(number + 1));
+    }
+    return result;
+}
 
 auto const constraints = std::vector<Constraint>{
-    {{.nationality = "English"}, HousePossibilities{.color = {"Red"}}},
-    {{.nationality = "English"}, HousePossibilities{.color = {"Red"}}},
-    {{.nationality = "Spaniard"}, HousePossibilities{.pet = {"Dog"}}},
-    {{.color = "Green"}, HousePossibilities{.drink = {"Coffee"}}},
-    {{.nationality = "Ukrainian"}, HousePossibilities{.drink = {"Tea"}}},
-    {{.color = "Green"}, right_house},
-    {{.pet = "Snail"}, HousePossibilities{.hobby = {"Dancing"}}},
-    {{.color = "Yellow"}, HousePossibilities{.hobby = {"Painting"}}},
-    {{.location = 2}, HousePossibilities{.drink = {"Milk"}}},
-    {{.nationality = "Norwegian"}, HousePossibilities{.location = {0}}},
-    {{.hobby = "Reading"}, adjacent_house},
-    {{.drink = "Orange"}, HousePossibilities{.hobby = {"Football"}}},
-    {{.nationality = "Japanese"}, HousePossibilities{.hobby = {"Chess"}}},
+    {
+        {nationality, "English"},
+        [](auto const&) { return ElementSet{{color, "Red"}}; },
+    },
+    {
+        {nationality, "English"},
+        [](auto const&) { return ElementSet{{color, "Red"}}; },
+    },
+    {
+        {nationality, "Spaniard"},
+        [](auto const&) { return ElementSet{{pet, "Dog"}}; },
+    },
+    {
+        {color, "Green"},
+        [](auto const&) { return ElementSet{{drink, "Coffee"}}; },
+    },
+    {
+        {nationality, "Ukrainian"},
+        [](auto const&) { return ElementSet{{drink, "Tea"}}; },
+    },
+    {{color, "Green"}, right_house},
+    {
+        {pet, "Snail"},
+        [](auto const&) { return ElementSet{{hobby, "Dancing"}}; },
+    },
+    {
+        {color, "Yellow"},
+        [](auto const&) { return ElementSet{{hobby, "Painting"}}; },
+    },
+    {
+        {location, "3"},
+        [](auto const&) { return ElementSet{{drink, "Milk"}}; },
+    },
+    {
+        {nationality, "Norwegian"},
+        [](auto const&) { return ElementSet{{location, "1"}}; },
+    },
+    {{hobby, "Reading"}, adjacent_house},
+    {
+        {drink, "Orange"},
+        [](auto const&) { return ElementSet{{hobby, "Football"}}; },
+    },
+    {
+        {nationality, "Japanese"},
+        [](auto const&) { return ElementSet{{hobby, "Chess"}}; },
+    },
 };
 
 class Houses {
 public:
     auto apply(Constraint const& constraint) -> bool;
 
-    auto find(House const& house) const -> House;
+    auto find(HouseElement const& house) -> House&;
 
 private:
     std::vector<House> houses_;
 };
 
 auto Houses::apply([[maybe_unused]] Constraint const& constraint) -> bool {
+    auto& house = find(constraint.house);
+
+    auto const c = constraint.second(
+
     return false;
 }
 
-auto Houses::find(House const& house) const -> House {
-    return house;
+struct Contains {
+    HouseElement const& element;
+
+    auto operator()(House const& house) const {
+        return house.elements.contains(element.first)
+            && house.elements.at(element.first) == element.second;
+    }
+};
+
+auto Houses::find(HouseElement const& element) -> House& {
+    if (auto const iter = std::ranges::find_if(houses_, Contains{element});
+        iter != houses_.end()) {
+        return *iter;
+    }
+    houses_.push_back({.elements = {element}, .potential_elements = {}});
+    return houses_.back();
 }
 
 } // namespace
@@ -103,8 +154,8 @@ auto solve() -> Solution {
     } while (state_changed);
 
     return {
-        .drinksWater = houses.find(House{.drink = "Water"}).nationality,
-        .ownsZebra   = houses.find(House{.pet = "Zebra"}).nationality,
+        .drinksWater = houses.find({drink, "Water"}).elements.at(nationality),
+        .ownsZebra   = houses.find({pet, "Zebra"}).elements.at(nationality),
     };
 }
 

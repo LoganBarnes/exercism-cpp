613ece1b1a6efb2fdf5d60a365e852987e99cbcd
Logan Barnes <logan@rotor.ai>
2026-04-27 15:55:55 -0600

    Shortest

--- speedywagon/speedywagon.h
+++ speedywagon/speedywagon.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <algorithm>
-#include <functional>
 #include <numeric>
 #include <string>
 #include <vector>
@@ -18,13 +17,13 @@ inline auto uv_light_heuristic(std::vector<int> const* const data) {
     auto const sum = std::accumulate(data->begin(), data->end(), 0);
     auto const avg = static_cast<double>(sum) / static_cast<double>(data->size());
 
-    return static_cast<int>(std::ranges::count_if(*data, std::bind_front(std::less<double>{}, avg))) + 1;
+    return static_cast<int>(std::count_if(data->begin(), data->end(), [avg](auto const i) { return i > avg; })) + 1;
 }
 
 inline auto connection_check(pillar_men_sensor const* const s) { return nullptr != s; }
 
 inline auto activity_counter(pillar_men_sensor const* const s, size_t const len) {
-    return std::accumulate(s, s + len, 0, [](auto tot, auto const& sen) { return tot + sen.activity; });
+    return std::accumulate(s, s + len, 0, [](auto const tot, auto const& sen) { return tot + sen.activity; });
 }
 
 inline auto alarm_control(pillar_men_sensor const* const s) { return s && (s->activity > 0); }

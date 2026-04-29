ae59f230abd65fff3b9ea150ce1378bda066e566
Logan Barnes <logan@rotor.ai>
2026-04-29 22:27:42 +0900

    Works

--- knapsack/knapsack.cpp
+++ knapsack/knapsack.cpp
@@ -5,14 +5,10 @@
 namespace knapsack {
 namespace {
 
-using Key    = std::vector<bool>;
-using Totals = std::unordered_map<std::vector<bool>, Item>;
+using Knapsacks = std::vector<Item>;
 
-int iter = 0;
-
-auto best_sub_sack(
-    Totals&                  totals,
-    Key                      key,
+auto compute_knapsacks(
+    Knapsacks&               knapsacks,
     Item                     total,
     size_t const             index,
     int const                max_weight,
@@ -23,25 +19,16 @@ auto best_sub_sack(
     if (index >= item_count) {
         return;
     }
-    key[index] = true;
-
-    // unused?
-    if (totals.find(key) != totals.end()) {
-        return;
-    }
-
-    ++iter;
-
     auto const& item = items[index];
 
     total.weight += item.weight;
     total.value += item.value;
 
     if (total.weight <= max_weight) {
-        totals[key] = total;
+        knapsacks.push_back(total);
 
         for (auto i = index + 1; i < item_count; ++i) {
-            best_sub_sack(totals, key, total, i, max_weight, items);
+            compute_knapsacks(knapsacks, total, i, max_weight, items);
         }
     }
 }
@@ -50,23 +37,17 @@ auto best_sub_sack(
 
 auto maximum_value(int const max_weight, std::vector<Item> const& items)
     -> int {
-    auto const key = std::vector<bool>(items.size(), false);
-
-    auto totals = Totals{};
-    iter = 0;
+    auto knapsacks = Knapsacks{};
 
     for (auto i = 0U; i < items.size(); ++i) {
-        best_sub_sack(totals, key, {}, i, max_weight, items);
+        compute_knapsacks(knapsacks, {}, i, max_weight, items);
     }
 
-    std::cout << items.size() << ": " << iter << std::endl;
-
-    auto best_value = 0;
-    for (auto const& total : totals) {
-        best_value = std::max(best_value, total.second.value);
+    if (knapsacks.empty()) {
+        return 0;
     }
 
-    return best_value;
+    return std::max_element(knapsacks.begin(), knapsacks.end())->value;
 }
 
 } // namespace knapsack
